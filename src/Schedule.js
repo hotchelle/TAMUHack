f183756678c2c1406cba17c04133ef340acd31d9
Abdulla Sakallah <iaboodws11@gmail.com>
2023-01-29 11:12:58 -0600

    Update Schedule.js

--- src/Schedule.js
+++ src/Schedule.js
@@ -24,7 +24,7 @@ const Schedule = ({navigation}) => {
         </View>
       ))}
       <TouchableOpacity style={styles.button} onPress={()=> navigation.navigate("Rating")}>
-        <Text style={styles.buttonText}>Prayer Rate</Text>
+        <Text style={styles.buttonText}>Prayer Rating</Text>
       </TouchableOpacity>
      
     </View>
@@ -35,12 +35,15 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    paddingTop: 40
+    paddingTop: 40,
+    position: 'relative',
+    justifyContent: 'center',
   },
   title: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 20
+    marginBottom: 10
+    
   },
   scheduleItem: {
     width: '80%',
